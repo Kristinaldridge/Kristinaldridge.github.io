ea34dd5c0f1325befffc20dc747960edc2fab5bc
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-07-22 09:11:46 -0400

    added insurance plan component- redo project bc crashed app

---
+++ src/components/InsurancePlan.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function InsurancePlans (){
+return(
+    <section className ="insurance-plans">
+        <div className="container">
+
+            <h2>Our Insurance Plans</h2>
+                <div className="plan-card">
+                    <h3>Basic Plan</h3>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisciing elit.</p>
+                </div>
+                <div className="plan-card">
+                    <h3>Premium Plan</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+        </div>
+
+    </section>
+
+);
+};
+
+export default InsurancePlans
