a08b3701d63895d0f4b43a6bdf631517ddd343ed
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-07-22 09:10:01 -0400

    added footer from project that crashed redo app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header.js';
 // import BreedCard from './components/BreedCard.js';
-// import Footer from './components/Footer.js';
+import Footer from './components/Footer.js';
 // import MoneySection from './components/MoneySection.js';
 import './styles/index.css';
 // import InsurancePlans from './components/InsurancePlan.js';
