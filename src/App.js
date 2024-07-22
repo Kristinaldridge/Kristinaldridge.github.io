8c4e5606a00da710efd4686f8b4312621358e7e5
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-07-22 09:06:01 -0400

    added app js setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header.js';
+// import BreedCard from './components/BreedCard.js';
+// import Footer from './components/Footer.js';
+// import MoneySection from './components/MoneySection.js';
+import './styles/index.css';
+// import InsurancePlans from './components/InsurancePlan.js';
+
+function App() {
+    return (
+      <div className="App">
+        <Header />    
+
+  </div>
+    );
+  };
+
+  export default App;
