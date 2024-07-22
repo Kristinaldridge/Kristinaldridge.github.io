2b25dfdf674e449cac150070b9ffc26daccee078
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-07-22 09:15:32 -0400

    added app js section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header.js';
-// import BreedCard from './components/BreedCard.js';
+import BreedCard from './components/BreedCard.js';
 import Footer from './components/Footer.js';
-// import MoneySection from './components/MoneySection.js';
+import MoneySection from './components/MoneySection.js';
 import './styles/index.css';
-// import InsurancePlans from './components/InsurancePlan.js';
+import InsurancePlans from './components/InsurancePlan.js';
 
 function App() {
     return (
       <div className="App">
         <Header />    
-
+<main>
+<section>
+<div>
+      <BreedCard />
+    </div>
+  </section>
+  <div>
+    <InsurancePlans />
+  </div>
+<div>
+ <MoneySection />
+</div>
+  </main>
+  <Footer />
   </div>
     );
   };
