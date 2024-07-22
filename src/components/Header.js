ed73b12ce9e196b9a88c4b31234f05d9fdc21aae
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-07-22 09:07:39 -0400

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,38 @@
+import React, {useState} from 'react';
+// import {Link} from 'react-router-dom';
+
+
+const Header = () => {
+  // eslint-disable-next-line
+  const[showOffer, setShowOffer]= useState(true);
+
+  return (
+    <header>
+      <div className="container">
+      {showOffer &&(
+        <div className="floating-offer">
+          <p>20% off today with code: Bark20</p>
+        </div>
+      )}
+        <h1>Bark Insurance</h1>
+        <nav>
+          <ul>
+            
+             <li>Home</li>
+            <li>Insurance Plans</li>
+            <li>Quote</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+
+
+
 
 
 
